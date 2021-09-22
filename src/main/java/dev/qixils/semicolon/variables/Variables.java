bc0db9fd172ebcf33b1c2665f7894c71f500fefc
Lexi <lexi@qixils.dev>
2021-09-22 12:44:46 -0400

    Create & document AbstractVariables

--- src/main/java/dev/qixils/semicolon/variables/Variables.java
+++ src/main/java/dev/qixils/semicolon/variables/Variables.java
@@ -3,7 +3,6 @@ package dev.qixils.semicolon.variables;
 import dev.qixils.semicolon.Semicolon;
 import dev.qixils.semicolon.variables.parsers.ListParser;
 import dev.qixils.semicolon.variables.parsers.StringParser;
-import dev.qixils.semicolon.variables.parsers.VariableParser;
 import dev.qixils.semicolon.variables.parsers.numbers.ByteParser;
 import dev.qixils.semicolon.variables.parsers.numbers.NumberParser;
 import dev.qixils.semicolon.variables.parsers.snowflakes.ChannelParser;
@@ -13,27 +12,10 @@ import dev.qixils.semicolon.variables.parsers.snowflakes.UserParser;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
-public final class Variables {
-	private final Map<String, VariableParser<?>> REGISTRY = new HashMap<>();
-
-	@NotNull
-	private <T extends VariableParser<?>> T register(@NotNull String variable, @NotNull T parser) {
-		REGISTRY.put(Objects.requireNonNull(variable, "variable"),
-					 Objects.requireNonNull(parser, "parser"));
-		return parser;
-	}
-
-	@Nullable
-	public VariableParser<?> get(@NotNull String variable) {
-		return REGISTRY.get(Objects.requireNonNull(variable, "variable cannot be null"));
-	}
-
+public final class Variables extends AbstractVariables {
 	public final ListParser<String> PREFIX;
 	public final ChannelParser<TextChannel> STARBOARD_CHANNEL;
 	public final ByteParser STARBOARD_THRESHOLD;
@@ -51,26 +33,26 @@ public final class Variables {
 	public final ChannelParser<TextChannel> MODMAIL_CHANNEL;
 	public final ChannelParser<TextChannel> ROLES_CHANNEL;
 	//public final ChannelParser<TextChannel> MOD_LOG_CHANNEL;
-	public final ListParser<User> IGNORED_MESSAGES_LOG_USERS; // TODO: rename
+	public final ListParser<User> IGNORED_MESSAGE_LOG_USERS;
 
 	public Variables(@NotNull Semicolon bot) {
 		Objects.requireNonNull(bot, "bot cannot be null");
-		PREFIX = new ListParser<>(bot, new StringParser(bot), ' ');
-		STARBOARD_CHANNEL = new ChannelParser<>(bot, TextChannel.class);
-		STARBOARD_THRESHOLD = new ByteParser(bot, NumberParser.ParserFilter.ONLY_POSITIVE);
-		STARBOARD_EMOJI = new StringParser(bot);
-		MUTED_ROLE = new RoleParser(bot);
-		JOIN_ROLE = new RoleParser(bot);
-		REPORT_EMOTE = new EmoteParser(bot);
-		PING_BAN_THRESHOLD = new ByteParser(bot, NumberParser.ParserFilter.NON_NEGATIVE);
-		PING_BAN_JOIN_BYPASS = new ByteParser(bot, NumberParser.ParserFilter.NON_NEGATIVE);
-		PING_BAN_CREATION_BYPASS = new ByteParser(bot, NumberParser.ParserFilter.NON_NEGATIVE);
-		MISBEHAVIOR_THRESHOLD = new ByteParser(bot, NumberParser.ParserFilter.NON_NEGATIVE);
-		JOINS_CHANNEL = new ChannelParser<>(bot, TextChannel.class);
-		MESSAGES_CHANNEL = new ChannelParser<>(bot, TextChannel.class);
-		NAME_CHANGE_CHANNEL = new ChannelParser<>(bot, TextChannel.class);
-		MODMAIL_CHANNEL = new ChannelParser<>(bot, TextChannel.class);
-		ROLES_CHANNEL = new ChannelParser<>(bot, TextChannel.class);
-		IGNORED_MESSAGES_LOG_USERS = new ListParser<>(bot, new UserParser(bot), ',');
+		PREFIX = register("prefix", new ListParser<>(bot, new StringParser(bot), ' '));
+		STARBOARD_CHANNEL = register("starboard_channel", new ChannelParser<>(bot, TextChannel.class));
+		STARBOARD_THRESHOLD = register("starboard_threshold", new ByteParser(bot, NumberParser.ParserFilter.ONLY_POSITIVE));
+		STARBOARD_EMOJI = register("starboard_emoji", new StringParser(bot));
+		MUTED_ROLE = register("muted_role", new RoleParser(bot));
+		JOIN_ROLE = register("join_role", new RoleParser(bot));
+		REPORT_EMOTE = register("report_emote", new EmoteParser(bot)); // TODO: handle rename in legacy import
+		PING_BAN_THRESHOLD = register("ping_ban_threshold", new ByteParser(bot, NumberParser.ParserFilter.NON_NEGATIVE));
+		PING_BAN_JOIN_BYPASS = register("ping_ban_join_bypass", new ByteParser(bot, NumberParser.ParserFilter.NON_NEGATIVE));
+		PING_BAN_CREATION_BYPASS = register("ping_ban_creation_bypass", new ByteParser(bot, NumberParser.ParserFilter.NON_NEGATIVE));
+		MISBEHAVIOR_THRESHOLD = register("misbehavior_threshold", new ByteParser(bot, NumberParser.ParserFilter.NON_NEGATIVE));
+		JOINS_CHANNEL = register("joins_channel", new ChannelParser<>(bot, TextChannel.class));
+		MESSAGES_CHANNEL = register("messages_channel", new ChannelParser<>(bot, TextChannel.class));
+		NAME_CHANGE_CHANNEL = register("name_change_channel", new ChannelParser<>(bot, TextChannel.class));
+		MODMAIL_CHANNEL = register("modmail_channel", new ChannelParser<>(bot, TextChannel.class));
+		ROLES_CHANNEL = register("roles_channel", new ChannelParser<>(bot, TextChannel.class));
+		IGNORED_MESSAGE_LOG_USERS = register("ignored_message_log_users", new ListParser<>(bot, new UserParser(bot), ','));  // TODO: handle rename in legacy import
 	}
 }
