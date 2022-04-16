3414c3ff53f0028438133d79d3de7d1932777b31
endorpersand <daendrdragon@gmail.com>
2022-04-16 15:30:05 -0400

    add perms/botperms permission checks

---
+++ src/main/java/dev/qixils/quasicolon/error/permissions/UserMissingPermException.java
@@ -0,0 +1,17 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package dev.qixils.quasicolon.error.permissions;
+
+import dev.qixils.quasicolon.Key;
+import dev.qixils.quasicolon.text.Text;
+import net.dv8tion.jda.api.Permission;
+
+public class UserMissingPermException extends NoPermissionException {
+	public UserMissingPermException(Permission perm) {
+		super(Text.single(Key.library("exception.user_missing_perm"), perm));
+	}
+}
