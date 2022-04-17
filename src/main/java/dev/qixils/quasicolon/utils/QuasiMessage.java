8d0911c7cffabf78386d8ebf4008433470b7b30e
endorpersand <daendrdragon@gmail.com>
2022-04-16 20:29:08 -0400

    add javadocs for QuasiMessage

--- src/main/java/dev/qixils/quasicolon/utils/QuasiMessage.java
+++ src/main/java/dev/qixils/quasicolon/utils/QuasiMessage.java
@@ -7,5 +7,10 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import dev.qixils.quasicolon.text.Text;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
 
-public record QuasiMessage(@NonNull Text text, @NonNull Consumer<MessageAction> modifier) {
-}
+/**
+ * Object that stores {@link Text} to send and modifiers 
+ * to the {@link MessageAction} created in sending the message.
+ * 
+ * This wrapper is useful for sending embeds or attachments.
+ */
+public record QuasiMessage(@NonNull Text text, @NonNull Consumer<MessageAction> modifier) { }
