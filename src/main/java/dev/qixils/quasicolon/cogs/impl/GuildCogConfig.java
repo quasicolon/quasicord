d64e895e3eea189b9b4ef7cbd577788a8898001f
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2022-12-11 19:35:21 -0800

    cogs

--- src/main/java/dev/qixils/quasicolon/cogs/impl/GuildCogConfig.java
+++ src/main/java/dev/qixils/quasicolon/cogs/impl/GuildCogConfig.java
@@ -1,4 +1,12 @@
 package dev.qixils.quasicolon.cogs.impl;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+@ConfigSerializable
+@Getter
+@NoArgsConstructor
 public class GuildCogConfig {
+	protected final boolean enabled = false;
 }
